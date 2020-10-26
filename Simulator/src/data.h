9285a79ee363220dfac31a25bdb74d615a9b30c9
Zvonimir Pervan <z.pervan65@gmail.com>
2020-10-26 19:23:05 +0100

    Simulator: Add logic for rule creation

---
+++ Simulator/src/data.h
@@ -0,0 +1,13 @@
+#ifndef GAMEOFLIFE_SIMULATOR_SRC_DATA_H_
+#define GAMEOFLIFE_SIMULATOR_SRC_DATA_H_
+
+#include <vector>
+#include <string>
+#include <bitset>
+
+namespace Data
+{
+	static std::vector<std::pair<std::string, std::bitset<8>>> rules;
+}
+
+#endif //GAMEOFLIFE_SIMULATOR_SRC_DATA_H_
